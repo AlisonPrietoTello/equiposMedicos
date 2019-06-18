5a4c8986927bd3d7d42d4f3d3e1d1e7f851619f9
AlisonPrietoTello <aliprietot@gmail.com>
2019-06-18 14:48:13 -0500

    version10

--- src/app/views/admin/equipos/equipos.component.ts
+++ src/app/views/admin/equipos/equipos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AppService } from '../../../app.service';
 import { Subject } from 'rxjs';
 
@@ -12,6 +12,20 @@ export class EquiposComponent implements OnInit {
   public loading:boolean;
   public protocolo:any;
   public parametro:any;
+  public equipon:any;
+  //input
+  @Input()
+  public codigo:any;
+  @Input()
+  public referencia:any;
+  @Input()
+  public nombre:any;
+  @Input()
+  public fkProtocolo:any;
+  @Input()
+  public fktipo:any;
+
+
   constructor(public service: AppService) {
     this.loading=true;
    }
@@ -19,11 +33,6 @@ export class EquiposComponent implements OnInit {
   public listarProtocolo(){
     this.service.getProtocolos().subscribe(
       result => {console.log(result),this.protocolo=result;
-        if(!this.service){
-          alert('Error en el servidor');
-        }else{  
-          this.loading=false;
-        }
       } );
 
   }
@@ -45,6 +54,22 @@ export class EquiposComponent implements OnInit {
       result=>{console.log(result),this.parametro=result;
       }
     )
+  }
+  //Agregar nuevo equipo 
+  public agregarEquipo(){
+    this.equipon={
+      "nombre":this.nombre,
+      "codigo":this.codigo,
+      "referencia":this.referencia,
+      "fkProtocolo":this.fkProtocolo,
+      "fkTipo":this.fktipo
+    } 
+    this.service.post('equipos/new', this.equipon).subscribe(
+      result=>{console.log(result)
+        this.listarEquipos();
+      }
+    )
+   
   }
     // Modal
     abrirModal() {
